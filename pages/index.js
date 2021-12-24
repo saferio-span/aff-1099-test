57e8b770c9fca0febd745cacb663ba7a90be3fc3
stsjohnsaferio <stsjohnsaferio@dotnetethics.com>
2021-12-24 16:01:12 +0530

    Added GTM

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,9 @@ export default function Home() {
   return (
     <>
       <Head>
+      {/* Google Tag Manager  */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PGZQDH6');`}}></script>
+      {/* End Google Tag Manager */}
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>
@@ -48,6 +51,10 @@ export default function Home() {
 
       </Head>
       <body id="page-top" data-spy="scroll">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGZQDH6" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript)  */}
+        
         <header id="header" className="fixed-top ">
         <nav className="navbar navbar-toggleable navbar-expand-lg navbar-inverse fixed-top mainmenu">
             <div className="container-fluid align-items-right">
